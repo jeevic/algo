efa5eaeabd25c6fc344fd2d0a94497a5b7d91190
caojianwei <caojianwei@yidian-inc.com>
2021-09-26 23:14:34 +0800

    add: listCycle141

---
+++ leetcode/linklist/list_cycle141/list_cycle.go
@@ -0,0 +1,28 @@
+package list_cycle141
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	p1, p2 := head, head
+	for p1 != nil && p2 != nil && p1.Next != nil && p2.Next != nil && p2.Next.Next != nil {
+		p1 = p1.Next
+		p2 = p2.Next.Next
+		if p1 == p2 {
+			return true
+		}
+	}
+	return false
+}
