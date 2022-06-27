9f308bc5a43ceff8b807d27ee370c9d210d68964
caojianwei <caojianwei@yidian-inc.com>
2022-06-27 22:51:22 +0800

    add: lfu && lru 算法

---
+++ datastruct/lfu_cache460/__init__.py
@@ -0,0 +1,77 @@
+
+class LFUCache:
+    kv_map = {}
+    kc_map = {}
+    freq_keys_map = {}
+    min_freq = 0
+    size = 0
+
+    def __init__(self, capacity: int):
+        self.kv_map = {}
+        self.kc_map = {}
+        self.freq_keys_map = {}
+        self.min_freq = 0
+        self.capacity = capacity
+        self.size = 0
+
+    def get(self, key: int) -> int:
+        if self.kv_map.get(key, None) is None:
+            return -1
+        self.incr_freq(key)
+        return self.kv_map[key]
+
+    def put(self, key: int, value: int) -> None:
+        if self.capacity <= 0:
+            return -1
+        if self.kv_map.get(key, None) is not None:
+            self.kv_map[key] = value
+            self.incr_freq(key)
+            return
+
+        if self.size >= self.capacity:
+            self.remove_least_freq_key()
+
+        self.kv_map[key] = value
+        self.kc_map[key] = 1
+        self.min_freq = 1
+        freq_keys = self.freq_keys_map.get(self.min_freq, [])
+        freq_keys.append(key)
+        self.freq_keys_map[self.min_freq] = freq_keys
+        self.size += 1
+        return
+
+    def remove_least_freq_key(self):
+        lfreq = self.min_freq
+        keys = self.freq_keys_map[lfreq]
+        # 删除最左侧
+        key = keys.pop(0)
+        self.freq_keys_map[lfreq] = keys
+
+        if len(keys) == 0:
+            del self.freq_keys_map[lfreq]
+
+        del self.kc_map[key]
+        del self.kv_map[key]
+        self.size -= 1
+        return
+
+    def incr_freq(self, key):
+        freq = self.kc_map[key]
+        self.kc_map[key] += 1
+        freq_keys = self.freq_keys_map[freq]
+
+        freq_keys.remove(key)
+        self.freq_keys_map[freq] = freq_keys
+
+        if len(freq_keys) == 0:
+            self.min_freq = freq + 1
+
+        new_freq_keys = self.freq_keys_map.get(freq + 1, [])
+        new_freq_keys.append(key)
+        self.freq_keys_map[freq + 1] = new_freq_keys
+        return
+
+    # Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
---
+++ datastruct/lru-cache146/__init__.py
@@ -0,0 +1,94 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.map = {}
+        self.dbl = DobuleList()
+
+    def get(self, key: int) -> int:
+        if key not in self.map:
+            return -1
+        self.make_recent(key)
+        return self.map[key].val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.map:
+            self.deleteKey(key)
+            self.add_recent(key, value)
+            return
+
+        if self.dbl.get_size() == self.capacity:
+            self.remove_least_recent()
+        self.add_recent(key, value)
+        return
+
+    def remove_least_recent(self):
+        node = self.dbl.remove_first()
+        if node is None:
+            return
+        del self.map[node.key]
+
+    def make_recent(self, key):
+        node = self.map[key]
+        self.dbl.remove_node(node)
+        self.dbl.add_last(node)
+        return
+
+    def deleteKey(self, key):
+        node = self.map.get(key)
+        self.dbl.remove_node(node)
+        del self.map[key]
+
+    def add_recent(self, key, val):
+        node = Node(key, val)
+        self.dbl.add_last(node)
+        self.map[key] = node
+
+
+class DobuleList:
+
+    def __init__(self):
+        self.root = Node(None, None)
+        self.tail = Node(None, None)
+        self.size = 0
+
+        self.root.next = self.tail
+        self.tail.prev = self.root
+
+    def remove_node(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+        self.size -= 1
+        return
+
+    def add_last(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
+        self.size += 1
+        return
+
+    def remove_first(self):
+        if self.root.next == self.tail:
+            return None
+
+        first = self.root.next
+        self.root.next = first.next
+        first.next.prev = self.root
+        self.size -= 1
+        return first
+
+    def get_size(self):
+        return self.size
+
+
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
