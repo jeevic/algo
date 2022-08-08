f8f6e8094c4632cc30e7932e6c0a0f0a55874dbd
caojianwei <caojianwei@yidian-inc.com>
2022-08-08 19:59:53 +0800

    add: 股票最佳买卖

---
+++ leetcode/dp/best-time-to-buy-and-sell-stock/bestTimeToBuyAndSellStock122/max_profit.py
@@ -0,0 +1,26 @@
+"""
+122. 买卖股票的最佳时机 II
+"""
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        length = len(prices)
+        if length <= 0:
+            return 0
+        dp = [[0, 0] for _ in range(length)]
+
+        '''
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+        '''
+        for i, p in enumerate(prices):
+            if i - 1 == -1:
+                dp[i][0] = 0
+                dp[i][1] = -prices[i]
+                continue
+
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+
+        return dp[length - 1][0]
---
+++ leetcode/dp/best-time-to-buy-and-sell-stock/readme.md
@@ -0,0 +1,3 @@
+### 买卖股票的问题
+121. https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+122. https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/
