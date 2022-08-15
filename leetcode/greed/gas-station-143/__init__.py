b20bc8e3e3cd9f7fc5ea4a2602cd43795601b53d
caojianwei <caojianwei@yidian-inc.com>
2022-08-15 22:50:16 +0800

    add: 134. 加油站

--- leetcode/greed/gas-station-143/__init__.py
+++ leetcode/greed/gas-station-143/__init__.py
@@ -23,3 +23,29 @@ class Solution:
                 return i
 
         return -1
+
+
+"""
+sum += gas[i] - cost[i]
+小于0的最低点后就开始增长  此时为开始点最合适
+
+"""
+
+
+class Solution1:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+
+        sum1 = 0
+        min_sum = 0
+        start = 0
+        for i in range(n):
+            sum1 += gas[i] - cost[i]
+            if sum1 < min_sum:
+                min_sum = sum1
+                start = i + 1
+        if sum1 < 0:
+            return -1
+
+        return start if start != n else 0
+
