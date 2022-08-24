12b772de4f6a5e2f86cdb26e1dfe082508d3b299
caojianwei <caojianwei@yidian-inc.com>
2022-08-24 23:53:55 +0800

    add: 323. 无向图中连通分量的数目

--- leetcode/graph/surrounded-regions-130/__init__.py
+++ leetcode/graph/surrounded-regions-130/__init__.py
@@ -45,3 +45,84 @@ class Solution:
         for x, y in dt:
             self.dfs(board, i + x, j + y)
         return
+
+
+"""
+并查集  unionfind
+将边界所有O 映射到 dummy 上
+将所有相连O联通
+遍历所有O不和 dummy 联通置为 X
+"""
+
+
+class Solution3:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m = len(board)
+        n = len(board[0])
+
+        uf = UnionFind(m * n + 1)
+        dummy = m * n
+
+        for i in range(m):
+            if board[i][0] == "O":
+                uf.union(i * n, dummy)
+            if board[i][n - 1] == "O":
+                uf.union(i * n + n - 1, dummy)
+
+        for j in range(n):
+            if board[0][j] == "O":
+                uf.union(j, dummy)
+            if board[m - 1][j] == "O":
+                uf.union((m - 1) * n + j, dummy)
+
+        d = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == "O":
+                    for x, y in d:
+                        if 0 <= i + x < m and 0 <= j + y < n:
+                            if board[i + x][j + y] == "O":
+                                uf.union(i * n + j, (i + x) * n + j + y)
+
+        for i in range(m):
+            for j in range(n):
+                if not uf.connected(dummy, i * n + j):
+                    board[i][j] = "X"
+
+        return board
+
+
+class UnionFind:
+
+    def __init__(self, n):
+        self._count = n
+        self._parent = [i for i in range(n)]
+
+    def union(self, p, q):
+        parent_p = self.find(p)
+        parent_q = self.find(q)
+
+        if parent_p == parent_q:
+            return
+        self._parent[parent_p] = self._parent[parent_q]
+        self._count -= 1
+        return
+
+    def connected(self, p, q):
+        parent_p = self.find(p)
+        parent_q = self.find(q)
+        if parent_p == parent_q:
+            return True
+        return False
+
+    def find(self, x):
+        while self._parent[x] != x:
+            self._parent[x] = self.find(self._parent[x])
+            x = self._parent[x]
+        return self._parent[x]
+
+    def count(self):
+        return self._count
